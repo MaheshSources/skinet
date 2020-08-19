8dcb182c668285b55eddfdff46d87c9436558b68
Mahesh <mahesh.rachamadugu@hexagon.com>
2020-08-19 12:55:05 +0530

    End of Section3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,8 +15,20 @@ namespace Infrastructure.Data
 
     }
 
-  public DbSet<Product> Products { get; set; }
+    public DbSet<Product> Products { get; set; }
 
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+
+    public DbSet<ProductType> ProductTypes { get; set; }
+
+
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
 
   }
 
