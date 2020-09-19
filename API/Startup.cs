fc7b80cefa451820305092d47341d43ce85ddccb
Mahesh <mahesh.rachamadugu@hexagon.com>
2020-09-19 17:44:03 +0530

    End of Section15

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using AutoMapper;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +34,14 @@ namespace API
       services.AddDbContext<StoreContext>(x =>
                          x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
+      services.AddDbContext<AppIdentityDbContext>(
+        x =>
+        {
+          x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+        });
+
+
+
       services.AddSingleton<IConnectionMultiplexer>(c =>
       {
         var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
@@ -40,6 +49,7 @@ namespace API
       });
 
       services.AddApplicationServices();
+      services.AddIdentityServices(_config);
       services.AddSwaggerDocumentation();
 
       services.AddCors(opt =>
@@ -55,25 +65,27 @@ namespace API
 
     }
 
-  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-      {
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
 
-        app.UseMiddleware<ExceptionMiddleware>();
-        // if (env.IsDevelopment())
-        // {
-        //   app.UseDeveloperExceptionPage();
-        // }
+      app.UseMiddleware<ExceptionMiddleware>();
+      // if (env.IsDevelopment())
+      // {
+      //   app.UseDeveloperExceptionPage();
+      // }
 
-        app.UseStatusCodePagesWithReExecute("/errors/{0}");
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-        app.UseHttpsRedirection();
+      app.UseHttpsRedirection();
 
-        app.UseRouting();
+      app.UseRouting();
 
-        app.UseStaticFiles();
+      app.UseStaticFiles();
 
       app.UseCors("CorsPolicy");
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
@@ -83,6 +95,6 @@ namespace API
         {
           endpoints.MapControllers();
         });
-      }
     }
   }
+}
