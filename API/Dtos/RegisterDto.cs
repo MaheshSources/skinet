7aa15c2e400ee2db55a820b18b561ac38261ccf7
Mahesh <mahesh.rachamadugu@hexagon.com>
2020-09-19 19:42:01 +0530

    End of section16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,14 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
-    public class RegisterDto
-    {
-        public string DisplayName { get; set; }
+  public class RegisterDto
+  {
+    [Required] public string DisplayName { get; set; }
 
-        public string Email  { get; set; }
+    [Required] [EmailAddress] public string Email { get; set; }
 
-        public string Password { get; set; }
+    [Required] [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage = "Password must have 1 Uppercase, 1 lower case, 1 number, 1 alpha numeric with 6 characters")]
+     public string Password { get; set; }
 
 
 
-    }
+  }
 }
--- API/Extensions/SwaggerServiceExtension.cs
+++ API/Extensions/SwaggerServiceExtension.cs
@@ -17,6 +17,23 @@ namespace API.Extensions
              Title = "SkiNet API",
              Version = "v1"
            });
+
+           var securitySchema = new OpenApiSecurityScheme
+           {
+             Description = "JWT Auth Bearer Scheme",
+             Name = "Authorization",
+             In = ParameterLocation.Header,
+             Type = SecuritySchemeType.Http,
+             Scheme = "bearer",
+             Reference = new OpenApiReference
+             {
+               Type = ReferenceType.SecurityScheme,
+               Id = "Bearer"
+             }
+           };
+           c.AddSecurityDefinition("Bearer", securitySchema);
+           var SecurityRequirements = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+           c.AddSecurityRequirement(SecurityRequirements);
          });
 
       return services;
@@ -26,11 +43,11 @@ namespace API.Extensions
 
     public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
     {
-         app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-          c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");
-        });
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");
+      });
 
 
 
